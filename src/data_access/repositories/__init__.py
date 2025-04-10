b5d495d419b8254b8bd7fb45f102f93244bf33dd
Cyril PERIE <cyril.perie96@gmail.com>
2025-04-10 08:42:34 +0200

    feat: one import file for repositories.

---
+++ src/data_access/repositories/__init__.py
@@ -0,0 +1,9 @@
+from src.data_access.repositories.race_repository import RaceRepository
+from src.data_access.repositories.horse_repository import HorseRepository
+from src.data_access.repositories.result_repository import ResultRepository
+
+__all__ = [
+    'RaceRepository',
+    'HorseRepository',
+    'ResultRepository'
+]
